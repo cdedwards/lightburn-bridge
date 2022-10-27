a992d68cde7224a33c90dd56d3b8b07bdffb9c8f
Brandon Nance <bwnance@gmail.com>
2022-10-27 00:26:23 -0400

    a test

--- LBBridge/relay.py
+++ LBBridge/relay.py
@@ -8,13 +8,14 @@ import socket, select, signal, threading, time
 from enum import Enum
 from . import log
 
+
 class RelayType(Enum):
     Ethernet = 1
     Serial = 2
+    TestEnum = 3
 
 
 class Relay:
-
     def __init__(self, cfg, status):
         self.version = (0, 0)
         self.cfg = cfg
@@ -23,22 +24,21 @@ class Relay:
         self.stop = False
         self.type = RelayType.Ethernet
         self.laser_ip = self.cfg.laser_ip
-        self.server_ip = '0.0.0.0'
+        self.server_ip = "0.0.0.0"
         self.server_port = 5005
         signal.signal(signal.SIGINT, self.stop)
         signal.signal(signal.SIGTERM, self.stop)
 
     def run(self):
-        log.error('Cannot call run() on base Relay class')
+        log.error("Cannot call run() on base Relay class")
 
     def stop(self):
-        log.info('Relay service requested to stop')
+        log.info("Relay service requested to stop")
         with self.stop_lock:
             self.stop = True
 
 
 class RuidaRelay(Relay):
-
     def __init__(self, cfg, status):
         super().__init__(cfg, status)
         self.version = (1, 0)
@@ -50,24 +50,26 @@ class RuidaRelay(Relay):
     def runConnection(self, sock):
         outSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         inSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        inSock.bind(('', self.from_laser_port))
+        inSock.bind(("", self.from_laser_port))
         inSock.setblocking(0)
         serv, addr = sock.accept()
         self.status.ok(f"Connection from: {addr[0]}:{addr[1]}")
         serv.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
         serv.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 4096)
         serv.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 32768)
-        packet = b''
+        packet = b""
         packetLen = 0
         lastLen = 0
-        packetType = ord('L')
-        ackValue = b''
+        packetType = ord("L")
+        ackValue = b""
         gotAck = True
         lastTime = time.time()
         while 1:
-            readable, writable, exceptional = select.select([inSock, serv], [], [serv], 1.0)
+            readable, writable, exceptional = select.select(
+                [inSock, serv], [], [serv], 1.0
+            )
             if serv in exceptional:
-                self.status.error('Server socket error')
+                self.status.error("Server socket error")
                 break
             if gotAck:
                 if serv in readable:
@@ -90,19 +92,27 @@ class RuidaRelay(Relay):
                                 packet = serv.recv(packetLen)
                         if packetLen != 0:
                             if len(packet) == packetLen:
-                                if packetType == ord('L'):
-                                    outSock.sendto(packet, (self.laser_ip, self.to_laser_port))
+                                if packetType == ord("L"):
+                                    outSock.sendto(
+                                        packet,
+                                        (self.laser_ip, self.to_laser_port),
+                                    )
                                     lastLen = packetLen
                                     packetLen = 0
-                                    packet = b''
+                                    packet = b""
                                     lastTime = time.time()
                                     gotAck = False
                                 else:
-                                    if packetType == ord('P'):
-                                        data = b'P\x00\x02' + bytes(self.version)
+                                    if packetType == ord("P"):
+                                        data = b"P\x00\x02" + bytes(
+                                            self.version
+                                        )
                                         serv.send(data)
                                     else:
-                                        self.status.error(f"unhandled packet type {packetType}" + str(bytes(chr(packetType))))
+                                        self.status.error(
+                                            f"unhandled packet type {packetType}"
+                                            + str(bytes(chr(packetType)))
+                                        )
             if inSock in readable:
                 data, addr = inSock.recvfrom(self.buffer_size)
                 if len(data) > 1 or lastLen <= 500:
@@ -115,19 +125,19 @@ class RuidaRelay(Relay):
                         gotAck = True
                     else:
                         if ackValue[0] != data[0]:
-                            self.status.warn('Non-ack received')
+                            self.status.warn("Non-ack received")
                             break
                         else:
                             gotAck = True
             if gotAck == False:
                 if time.time() - lastTime > 6:
-                    self.status.error('Laser timeout error')
+                    self.status.error("Laser timeout error")
                     break
 
         serv.close()
         outSock.close()
         inSock.close()
-        self.status.ok('Ruida command complete')
+        self.status.ok("Ruida command complete")
 
     def run(self):
         self.stop = False
@@ -139,7 +149,9 @@ class RuidaRelay(Relay):
             sock.listen(1)
             gotConnect = False
             while True:
-                readable, writable, exceptional = select.select([sock], [], [sock], 1.0)
+                readable, writable, exceptional = select.select(
+                    [sock], [], [sock], 1.0
+                )
                 if sock in readable:
                     gotConnect = True
                     break
@@ -157,7 +169,7 @@ class RuidaRelay(Relay):
                 if self.stop:
                     break
 
-        self.status.info('Ruida relay stopped')
+        self.status.info("Ruida relay stopped")
 
 
-RELAYS = {'ruida': RuidaRelay}
+RELAYS = {"ruida": RuidaRelay}
